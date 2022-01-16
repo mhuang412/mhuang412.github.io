4cbee6af682d4e16bbf62048dcc8acfabd3eda09
Michelle Huang <mh52@princeton.edu>
2022-01-16 17:03:29 -0500

    commit local changes

--- src/components/Home/Techstack2.js
+++ src/components/Home/Techstack2.js
@@ -7,34 +7,34 @@ function DataSciIcons() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="logos:tensorflow" />   
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="logos:python" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="simple-icons:pandas" width="70" height="70" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="logos:numpy" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="simple-icons:scikitlearn" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="carbon:logo-r-script" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="logos:opencv" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="logos:matplotlib-icon" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="vscode-icons:file-type-jupyter" />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={1} className="tech-icons">
         <Icon icon="vscode-icons:file-type-excel" />
       </Col>
     </Row>
